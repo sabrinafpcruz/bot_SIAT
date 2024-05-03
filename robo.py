69c25a9d53762a0c1d2d179d05b0e60354f43fe2
sabrinafpcruz <sabrinafpcruz@gmail.com>
2024-05-03 11:31:48 -0300

    ateração do upload pelo excel

--- robo.py
+++ robo.py
@@ -6,11 +6,11 @@ from tkinter import *
 
 """while True:
     print(pyautogui.position())
-    time.sleep(1)"""       
+    time.sleep(1) """    
 
 #inserir dados
 numero_processo = None
-"""qtd_volumes = None
+qtd_volumes = None
 
 def __init__():
     pass
@@ -46,28 +46,41 @@ botao = customtkinter.CTkButton(janela, text='Enviar', command=ok)
 botao.pack(padx=10, pady=10)
 
 janela.mainloop()
-"""
+
+#alerta para o codigo começar
 pyautogui.alert('O código vai começar. Não utilize nada do computador até o código finalizar!')
 pyautogui.PAUSE = 1.0
 
 # Digitar número de processo no SIAT e consultar
-pyautogui.click(1482, 273)
+pyautogui.click(529, 225)
 pyautogui.press('tab')
 pyautogui.write(str(numero_processo))
+pyautogui.press('tab')
 pyautogui.press('enter')
 
 #selecionar o tipo de arquivo
 pyautogui.press('tab', presses=2)
 pyautogui.press(['down', 'down'])
+
+#Fazer upload do arquivo """excel"""
+
+#Salvar arquivo
+pyautogui.press('tab', presses=2)
 pyautogui.press('enter')
 
-#Fazer upload do arquivo
-pyautogui.click(402, 581)
-pyautogui.press('tab', presses=9)
-pyautogui.press('space')
-pyautogui.press('enter')
+#selecionar mais arquivos """excel"""
 
-#enviar arquivo
+
+"""for a in range(qtd_arquivos):
+    #selecionar mais arquivos
+    pyautogui.press('tab',presses=3)
+    pyautogui.press(['down', 'down'])
+    pyautogui.press('enter')
+    pyautogui.press('tab')
+    pyautogui.press('enter')
+    pyautogui.press('tab', presses=8)#quantidade de arquivos
+"""
+"""#enviar para historico
 pyautogui.click(798, 680)
 pyautogui.press('enter')
 time.sleep(1)
@@ -75,4 +88,4 @@ pyautogui.click(393,377)
 pyautogui.click(880, 683)
 pyautogui.press('enter')
 time.sleep(1)
-pyautogui.press('enter')
+pyautogui.press('enter')"""
