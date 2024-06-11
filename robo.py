60b4c01c74c83791b287b8c1407dc418b19fef1c
sabrinafpcruz <sabrinafpcruz@gmail.com>
2024-06-11 10:58:23 -0300

    Finzalização e alteração
    Alterando a forma como funciona a inserção dos arquivos na pagina web e finalizando o bot

--- robo.py
+++ robo.py
@@ -6,18 +6,44 @@ import tkinter as tk
 from tkinter import filedialog
 import pyperclip
 import os
+import pygetwindow as gw
+
+"2013.000195-8"
+"Auto de Infração"
+"AUTO DE INFRAÇÃO"
+"NOTIFICAÇÃO"
 
 #coordenadas do mouse
 """while True:
     print(pyautogui.position())
     time.sleep(1)        
 """
+"""
+print("Janelas encontradas:")
+for window in gw.getAllTitles():
+    print(window)
+"""
 # Variáveis globais
-numero_processo = None
 arquivo_excel = None
 valor_selecionado = ""
 contador_pdfs = 0
 caminho_corrigido = ""
+tipo_do_processo_normalizado = ""
+
+def normalizar_texto(texto):
+    substituicoes = {
+        "Ç": "c", "Ã": "a", "Á": "a", "É": "e", "Í": "i", "Ó": "o", "Ú": "u",
+        "Â": "a", "Ê": "e", "Î": "i", "Ô": "o", "Û": "u", "À": "a", "È": "e",
+        "Ì": "i", "Ò": "o", "Ù": "u", "Ä": "a", "Ë": "e", "Ï": "i", "Ö": "o",
+        "Ü": "u", "Ÿ": "y", "Ý": "y", "ç": "c", "ã": "a", "á": "a", "é": "e",
+        "í": "i", "ó": "o", "ú": "u", "â": "a", "ê": "e", "î": "i", "ô": "o",
+        "û": "u", "à": "a", "è": "e", "ì": "i", "ò": "o", "ù": "u", "ä": "a",
+        "ë": "e", "ï": "i", "ö": "o", "ü": "u", "ÿ": "y", "ý": "y"
+    }
+    texto_normalizado = texto
+    for chave, valor in substituicoes.items():
+        texto_normalizado = texto_normalizado.replace(chave, valor)
+    return texto_normalizado
 
 def selecionar_pasta():
     global caminho_corrigido
@@ -45,11 +71,6 @@ def contar_pdfs(diretorio):
             if file.lower().endswith('.pdf'):
                 contador_pdfs += 1
 
-def verificar_selecao(event):
-    global indice_selecionado, valor_selecionado
-    indice_selecionado = tipo_processo.current()  # Obtém o índice da seleção atual
-    valor_selecionado = tipo_processo.get()       # Obtém o valor da seleção atual
-
 def remover_caracteres(string_original):
     caracteres_indesejados = "-."
     for char in caracteres_indesejados:
@@ -57,7 +78,7 @@ def remover_caracteres(string_original):
     return string_original
 
 def ok():
-    global numero_processo  # Indica que estamos utilizando a variável global
+    global numero_processo, processo  # Indica que estamos utilizando a variável global
     processo = n_processo.get().strip()
 
     if processo:  # Verifica se os campos não estão vazios
@@ -65,21 +86,40 @@ def ok():
         janela.destroy()
     else:
         if not processo:
-            n_processo['bg'] = 'pink'
+            numero_processo['bg'] = 'pink'
             print('Preencha todos os campos!')
 
 def executar_tarefas():
     executar_contagem()
     ok()
 
+def processar_processo(arquivo):
+    pyautogui.click(576, 540)
+    pyautogui.press('down', presses=2)
+    pyautogui.press('enter')
+    time.sleep(1)
+    pyautogui.click(451, 574)
+    time.sleep(1)
+    pyautogui.press('tab', presses=9)
+    pyautogui.press('down', presses=arquivo)
+    pyautogui.press('space')
+    pyautogui.press('enter')
+    time.sleep(2)
+    pyautogui.press('tab', presses=2)
+    time.sleep(2)
+    pyautogui.press('enter')
+    time.sleep(3)
+    pyautogui.press('enter')
+    time.sleep(2)
+
 #Interface gráfica
 janela = customtkinter.CTk()
 janela.geometry('500x400')
 janela.resizable(False, False)
-texto = customtkinter.CTkLabel(janela, text='N° Processo')
+texto = customtkinter.CTkLabel(janela, text='Enviar processos')
 texto.pack(padx=10, pady=10)
 
-n_processo = customtkinter.CTkEntry(janela, placeholder_text='Número do Proceso')
+n_processo = customtkinter.CTkEntry(janela, placeholder_text='Ex: 2010.000000-0')
 n_processo.pack(padx=10, pady=10)
 
 texto = customtkinter.CTkLabel(janela, text='Escolha o caminho com os arquivos do processo')
@@ -91,12 +131,6 @@ caminho_processo.pack(padx=10, pady=10)
 caminho_entry = customtkinter.CTkEntry(janela, width=360)
 caminho_entry.pack(padx=40, pady=10)
 
-opcoes = ["Auto de Infração", "Notificação"]
-
-tipo_processo = customtkinter.CTkComboBox(janela, values=opcoes)
-tipo_processo.bind("<<ComboboxSelected>>", verificar_selecao)
-tipo_processo.pack(padx=10, pady=10)
-
 resultado_label = tk.Label(janela, text="")
 resultado_label.pack(padx=10, pady=10)
 
@@ -105,10 +139,9 @@ botao.pack(padx=10, pady=10)
 
 janela.mainloop()
 
-"""
 #Alerta para o código começar
 pyautogui.alert('O código vai começar. Não utilize nada do computador até o código finalizar!')
-pyautogui.PAUSE = 1.0
+time.sleep(2)
 
 #Digitar número de processo no SIAT e consultar
 pyautogui.click(529, 225)
@@ -116,137 +149,64 @@ pyautogui.press('tab')
 pyautogui.write(str(numero_processo))
 pyautogui.press('tab')
 pyautogui.press('enter')
+time.sleep(2)
 
-#Pegar o tipo do excel
-#Abrir Excel com processos
-pyautogui.hotkey('win', 'e')
-pyautogui.press('tab', presses=4)
-pyautogui.press('enter')
-pyautogui.write(r'\\suecia.pmjg.lan\serec\0 - COMPARTILHAMENTO SEREC\PROCESSOS ESCANEADOS E RENUMERADOS')
-pyautogui.press('enter')
-pyautogui.press('tab', presses=7)
-pyautogui.write('xlsx')
-pyautogui.press('tab', presses=2)
-pyautogui.press('space')
+#abrir arquivo explorar
+pyautogui.press('win')
+time.sleep(1)
+pyautogui.write('explorar')
 pyautogui.press('enter')
 time.sleep(3)
 
-#Pesquisar o processo
-pyautogui.hotkey('ctrl', 'l')
-pyautogui.write(str(n_processo))
-pyautogui.press('enter')
+#Digitar caminho excel
 pyautogui.press('tab',presses=4)
 pyautogui.press('enter')
+time.sleep(1)
+pyautogui.write(r'\\suecia.pmjg.lan\serec\0 - COMPARTILHAMENTO SEREC\PROCESSOS ESCANEADOS E RENUMERADOS')
+pyautogui.press('enter')
+time.sleep(1)
 
-#Pegar o tipo de arquivo
-pyautogui.press('right', presses=3)
-pyautogui.hotkey('Ctrl', 'c')
-pyautogui.hotkey('win', 'down')
-"Auto de Infração"
-"""
-tipo_do_processo=pyperclip.paste()
-time.sleep(7)
-pyautogui.click(626,546)
-print(tipo_do_processo)
-if tipo_do_processo == "Auto de Infração":
-        pyautogui.press('down', presses=19)
-        
-        #Clicar botão de upload dos processos
-        pyautogui.press('tab',presses=2)
-        pyautogui.press('enter')
-        time.sleep(1)
-
-        #Selecionar pasta
-        pyautogui.press('tab', presses=5)
-        pyautogui.press('enter')
-        pyautogui.write(caminho_corrigido)
-        pyautogui.press('enter')
-        time.sleep(2)
-
-        #Selecionar arquivo
-        pyautogui.press('tab',presses=4)
-        pyautogui.press('space')
-        pyautogui.press('enter')
-
-elif tipo_do_processo == "Notificação": #tipo_do_processo == "Notificação: "
-        pyautogui.press('down', presses=115)
-
-
-        #Clicar botão de upload dos processos
-        pyautogui.press('tab',presses=2)
-        pyautogui.press('enter')
-        time.sleep(2)
-
-        #Selecionar pasta
-        pyautogui.press('tab', presses=5)
-        pyautogui.write(str(caminho_corrigido))
-        pyautogui.press('enter')
-
-        #Selecionar arquivo
-        pyautogui.press('tab',presses=4)
-        pyautogui.press('space')
-        pyautogui.press('enter') 
-
-"""for arquivo in range(contador_pdfs-1):
-    #selecionar o tipo
-    if tipo_do_processo == "Auto de Infração":
-        pyautogui.press('down', presses=19)
-        #Clicar botão de upload dos processos
-        pyautogui.press('tab',presses=2)
-        pyautogui.press('enter')
-        time.sleep(2)
-
-        #Selecionar arquivo
-        pyautogui.press('tab',presses=4)
-        time.sleep(1)
-
-        pyautogui.press('space')
-        time.sleep(1)
-        pyautogui.press('enter')                
-
-
-    elif tipo_do_processo == "Notificação":
-        pyautogui.press('down', presses=115)
-        #Clicar botão de upload dos processos
-        pyautogui.press('tab', presses=2)
-        pyautogui.press('enter')
-        time.sleep(2)
-
-        #Selecionar pasta
-        pyautogui.press('tab', presses=5)
-        pyautogui.write(str(caminho_corrigido))
-        pyautogui.press('enter')
-
-        #Selecionar arquivo
-        pyautogui.press('tab',presses=4)
-        pyautogui.press('space')
-        pyautogui.press('enter')
-
-        #clicar em enviar
-        pyautogui.press('tab', presses=2)
-        pyautogui.press('enter')
-"""
-"""
-#Abrir chrome do SIAT
-
-#Selecionar o tipo de arquivo
-
-        #clicar em enviar
-        time.sleep(1)
-        pyautogui.press('tab', presses=2)
-        pyautogui.press('enter')
-
-#Salvar arquivo
+#Buscar Excel com os processos
+pyautogui.press('tab', presses=7)
+pyautogui.write('xlsx')
+time.sleep(2)
 pyautogui.press('tab', presses=2)
+pyautogui.press('space')
+pyautogui.press('enter')
+time.sleep(8)
+
+#Pesquisar processo
+pyautogui.hotkey('ctrl', 'l')
+time.sleep(1)
+pyautogui.write(str(processo))
+pyautogui.press('enter')
+time.sleep(2)
+pyautogui.press('tab', presses=4)
 pyautogui.press('enter')
 
-pyautogui.alert('Seus arquivos foram enviados com sucesso!')
-"""
+#Pegar tipo processo
+time.sleep(2)
+pyautogui.press('right', presses=3)
+pyautogui.hotkey('ctrl', 'c')
+time.sleep(1)
 
-""" #Selecionar arquivo
-        pyautogui.press('tab',presses=4)
-        time.sleep(1)
+pyautogui.getWindowsWithTitle("Excel")[0].minimize()
+pyautogui.getWindowsWithTitle("PROCESSOS ESCANEADOS E RENUMERADOS")[0].minimize()
+time.sleep(2)        
 
-        pyautogui.press('space')
-        time.sleep(1)
-        pyautogui.press('enter') """
+# Obtém o valor copiado para a área de transferência
+tipo_do_processo = pyperclip.paste()
+print(tipo_do_processo)
+# Imprime o valor após a substituição
+tipo_do_processo_normalizado = normalizar_texto(tipo_do_processo)
+
+print(tipo_do_processo_normalizado)
+print(contador_pdfs)
+
+# Execução do processo baseado no tipo de processo
+tipo_do_processo_normalizado == 'auto de infracao':
+print("Processo é 'auto de infracao'")
+for arquivo in range(contador_pdfs):
+    processar_processo(arquivo)
+
+pyautogui.alert('Todos os arquivos foram enviados com sucesso!')
